a5602263458fd735f9a3939b8e20cea891e8a5e1
Igor <igorlima.pf4@gmail.com>
2018-06-01 19:28:23 -0300

    Classe do Docente com os atributos e metodos getters e setters

--- app/src/main/java/br/edu/anhembi/gabaritando/Docente.java
+++ app/src/main/java/br/edu/anhembi/gabaritando/Docente.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+package br.edu.anhembi.gabaritando;
 
 public class Docente {
+
+    public Docente() {
+
+    }
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
