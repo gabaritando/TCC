f4b0630f5d6192ce983f26b90328848572628a24
Igor Lima <igor.lima@hypnobox.com.br>
2018-06-17 00:15:02 -0300

    retirando importacoes nao utilizadas

--- app/src/main/java/br/edu/anhembi/gabaritando/CameraPreview.java
+++ app/src/main/java/br/edu/anhembi/gabaritando/CameraPreview.java
@@ -2,14 +2,10 @@ package br.edu.anhembi.gabaritando;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.widget.ImageView;
-
 import org.opencv.android.JavaCameraView;
 import java.io.FileOutputStream;
 import java.io.IOException;
